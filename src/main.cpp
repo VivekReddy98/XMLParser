d9d542d239e99ac8705633611ee9b9a0434c009e
vkarri <vkarri@ncsu.edu>
2020-02-24 22:36:17 -0500

    Folder Structure Created

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+    int a = 10;
+    int b = 20;
+
+    std::cout << "Hello, world!" << std::endl;
+
+    b = 30;
+    int c = 40;
+
+    std::cout << "Hello, world Exit!" << std::endl;
+
+    return 0;
+}
